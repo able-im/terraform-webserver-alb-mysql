8bf708266aae835d6ee9b570288953ddc87b7297
Ubuntu <artemuhov@gmail.com>
2024-05-22 22:54:53 +0200

    Terraform + AWS + WebApp

---
+++ tf-files/variables.tf
@@ -0,0 +1,12 @@
+variable "git-name" {
+  default = "git-name"
+}
+variable "git-token" {
+  default = "ghp_"
+}
+variable "key-name" {
+  default = "keyfile"
+}
+variable "hosted-zone" {
+  default = "site.com"
+}
